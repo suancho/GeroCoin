704d6ad1a2cda5ca744ca6ecb583acbaf343883b
suancho <suanbio@gmail.com>
2017-06-07 17:56:54 +0900

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -2014,7 +2014,7 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1369623856; //epochtime
+        block.nTime    = 1300000000; //epochtime
         block.nBits    = 0x1e0ffff0;
         block.nNonce   = 1271350;
 
